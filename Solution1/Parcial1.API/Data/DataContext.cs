a7fa3ac74fdc4ad4367d1620b211639edf603f9f
Argenis3 <argenisss70@gmail.com>
2024-09-04 12:27:15 -0600

    Ultima actualizacion practica parcial 1

---
+++ Solution1/Parcial1.API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Parcial1.Shared.Entities;
+
+namespace Parcial1.API.Data
+{
+    public class DataContext:DbContext
+    {
+        DbSet<Vehicle> Vehicles { get; set; }
+        public DataContext(DbContextOptions<DataContext> options):base (options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Vehicle>().HasIndex(x => x.type).IsUnique();
+        }
+    }
+}
---
+++ Solution1/Parcial1.Shared/Entities/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1.Shared.Entities
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Vehiculo")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres")]
+        public string type { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Modelo")]
+        [MaxLength(200, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres")]
+        public string Model { get; set; } = null!;
+
+    }
+}
